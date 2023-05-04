6495916d4c884f48154f93235770721c16d8c1da
pawelok <pawelok35@gmail.com>
2023-05-04 20:24:56 +0200

    Update scripts for papier and nożyce

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Gracz wpisał: ' + playerInput);
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = 'nieznany ruch';
+let computerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
-  printMessage('Twój ruch to: ' + playerMove);
-}else if(playerInput == '2'){
-    playerMove = 'papier'
-    printMessage('Twój ruch to: ' + playerMove);
-}else(playerInput == '3'){
-    playerMove = 'nożyce'
-    printMessage('Twój ruch to: ' + playerMove); 
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}else if(randomNumber == 2){
+    computerMove = 'papier';
+}else(randomNumber == 3){
+    computerMove = 'nożyce';
 }
+
+printMessage('Mój ruch to: ' + computerMove);
+
 
