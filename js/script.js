befa3731408b1dc0ad8c2468e0f3a725d6511ba1
pawelok <pawelok35@gmail.com>
2023-05-04 20:13:40 +0200

    Add scripts for papier and nożyce

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-let computerMove = `kamień`;
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+  printMessage('Twój ruch to: ' + playerMove);
+}else if(playerInput == '2'){
+    playerMove = 'papier'
+    printMessage('Twój ruch to: ' + playerMove);
+}else(playerInput == '3'){
+    playerMove = 'nożyce'
+    printMessage('Twój ruch to: ' + playerMove); 
+}
+
